74b61f1520c9834a661775f29c157e3363ca55b6
AlecKap <120225785+AlecKap@users.noreply.github.com>
2025-04-23 17:08:04 -0600

    Set up basic fields for entities

---
+++ src/main/java/com/cooksys/lemonadestand/entities/Customer.java
@@ -0,0 +1,19 @@
+package com.cooksys.lemonadestand.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class Customer {
+  @Id
+  @GeneratedValue
+  private Long id;
+
+  @Column(nullable = false)
+  private String name;
+
+  @Column(nullable = false)
+  private String phoneNumber;
+}
---
+++ src/main/java/com/cooksys/lemonadestand/entities/Lemonade.java
@@ -0,0 +1,28 @@
+package com.cooksys.lemonadestand.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class Lemonade {
+  @Id
+  @GeneratedValue
+  private Long id;
+
+  @Column(nullable = false)
+  private double lemonJuice;
+
+  @Column(nullable = false)
+  private double water;
+
+  @Column(nullable = false)
+  private double sugar;
+
+  @Column(nullable = false)
+  private int iceCubes;
+
+  @Column(nullable = false)
+  private double price;
+}
