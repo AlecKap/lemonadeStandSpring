2160b1503808e5820dd59b3310ad1c778cecd57c
AlecKap <120225785+AlecKap@users.noreply.github.com>
2025-04-24 16:49:22 -0600

    Add Order, Customer, and LemonadeStand controllers

---
+++ src/main/java/com/cooksys/lemonadestand/controllers/CustomerController.java
@@ -0,0 +1,11 @@
+package com.cooksys.lemonadestand.controllers;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("customer")
+public class CustomerController {
+  
+}
