90ac6879be079126d1cf909ef55ada8e262fcb8b
AlecKap <120225785+AlecKap@users.noreply.github.com>
2025-04-24 17:54:43 -0600

    Update repositories and add Optional findById

--- src/main/java/com/cooksys/lemonadestand/repositories/OrderRepository.java
+++ src/main/java/com/cooksys/lemonadestand/repositories/OrderRepository.java
@@ -1,5 +1,7 @@
 package com.cooksys.lemonadestand.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,5 @@ import com.cooksys.lemonadestand.entities.Order;
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
-
+  Optional<Order> findById(Long id);
 }
