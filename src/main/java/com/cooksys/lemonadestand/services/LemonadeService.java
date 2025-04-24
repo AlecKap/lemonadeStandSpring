e487b14c11e6cc632cb120b9b8c75d0d8723f656
AlecKap <120225785+AlecKap@users.noreply.github.com>
2025-04-23 23:51:23 -0600

    Add LemonadeService interface

---
+++ src/main/java/com/cooksys/lemonadestand/services/LemonadeService.java
@@ -0,0 +1,12 @@
+package com.cooksys.lemonadestand.services;
+
+import java.util.List;
+
+import com.cooksys.lemonadestand.entities.Lemonade;
+
+public interface LemonadeService {
+
+  List<Lemonade> getAllLemonades();
+
+  Lemonade createLemonade(Lemonade lemonade);
+}
