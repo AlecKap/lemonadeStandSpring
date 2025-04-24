4b0e6a1656eb4adb438f51099d480339ebbb8443
AlecKap <120225785+AlecKap@users.noreply.github.com>
2025-04-24 16:47:45 -0600

    Add put and delete mapping endpoints

--- src/main/java/com/cooksys/lemonadestand/controllers/LemonadeController.java
+++ src/main/java/com/cooksys/lemonadestand/controllers/LemonadeController.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -39,5 +41,15 @@ public class LemonadeController {
   @GetMapping("/{id}")
   public LemonadeResponseDto getLemonadeById(@PathVariable Long id) {
     return lemonadeService.getLemonadeById(id);
+  }
+
+  @PutMapping("/{id}")
+  public LemonadeResponseDto updateLemonade(@PathVariable Long id, @RequestBody LemonadeRequestDto lemonadeRequestDto) {
+    return lemonadeService.updateLemonade(id, lemonadeRequestDto);
+  }
+
+  @DeleteMapping("/{id}")
+  public LemonadeResponseDto deleteLemonade(@PathVariable Long id) {
+    return lemonadeService.deleteLemonade(id);
   }
 }
